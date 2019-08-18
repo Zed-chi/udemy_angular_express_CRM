ae665c631cb5aa44ad6ebafe43feb7ec644859b5
zed <zed@mail.ru>
2019-08-18 20:48:02 +0300

    added route controllers

---
+++ controllers/position.js
@@ -0,0 +1,10 @@
+module.exports.getByCategory = (req, res) => {}
+
+
+module.exports.delete = (req, res) => {}
+
+
+module.exports.create = (req, res) => {}
+
+
+module.exports.update = (req, res) => {}
