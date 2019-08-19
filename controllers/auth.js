14be510ea511a5886437699ec848e4d5b9f48671
zed <zed@mail.ru>
2019-08-19 17:47:55 +0300

    added registation method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const UserModel = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 module.exports.login = (req,res)=>{
     res.status(200).json({
         email: req.body.email,
@@ -6,6 +9,23 @@ module.exports.login = (req,res)=>{
 }
 
 
-module.exports.register = (req,res)=>{
-    res.status(200).json({"Register":true});
+module.exports.register = async function (req,res){
+    const salt = bcrypt.genSaltSync(10);
+    const email = req.body.email;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({email:email});
+    if (existingUser){
+        res.status(409).json({message:"User exists"});
+    } else {
+        try{
+            const user = new UserModel({
+                email: email,
+                password: bcrypt.hashSync(password, salt),
+            });
+            await user.save().then(()=> console.log("user created"));
+            res.status(201).json({message:"user created"});
+        } catch(err){
+            console.log(err);
+        }        
+    }
 }
