95b96edc7bee50275b15820e355a9f4926b26591
zed <zed@mail.ru>
2019-08-20 20:32:13 +0300

    added passport midlleware in  route

--- routes/position.js
+++ routes/position.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/position");
+const passport = require("passport");
 
 
-router.get("/:category", controller.getByCategory);
-router.post("/", controller.create);
-router.delete("/:id", controller.delete);
-router.patch("/:id", controller.update);
+router.get("/:category", passport.authenticate("jwt", {session:false}), controller.getByCategory);
+router.post("/", passport.authenticate("jwt", {session:false}), controller.create);
+router.delete("/:id", passport.authenticate("jwt", {session:false}), controller.delete);
+router.patch("/:id", passport.authenticate("jwt", {session:false}), controller.update);
 module.exports = router;
